a60a942813697f67de65da2a0e1e44755b3d705e
DPulnar <w69997@student.wsiz.edu.pl>
2023-11-06 12:25:38 +0100

    zad1

--- lab2/zad2.py
+++ lab2/zad2.py
@@ -1,18 +1,3 @@
-class kalkulator:
-
-    def __init__(self, x, y,dzialanie):
-        self.x = x
-        self.y = y
-        self.dzialanie = dzialanie
-    def oblicz(self):
-        if self.dzialanie == "dodawianie":
-            return x + y
-        elif self.dzialanie == "odejmowanie":
-            return x - y
-        elif self.dzialanie == "mnozenie":
-            return x * y
-        elif self.dzialanie == "dzielenie":
-            return x / y
 def PodajLiczby():
     x = float(input("podaj liczbe x: "))
     y = float(input("podaj liczbe y: "))
@@ -20,21 +5,21 @@ def PodajLiczby():
         while y==0:
             y = float(input("podaj liczbe y: "))
     return x,y
+
+
+def oblicz(dzialanie,x,y)
+    if dzialanie == "dodawianie":
+        return  x+y
+    elif dzialanie == "odejmowanie":
+        return x-y
+    elif dzialanie == "mnozenie":
+        return x * y
+    elif dzialanie == "dzielenie":
+        return  x/y
 def init():
     x,y = PodajLiczby()
     dzialanie = input("podaj działanie")
-    calc = kalkulator(x,y,dzialanie)
-    wynik = calc.oblicz()
+    wynik = oblicz(dzialanie,x,y)
+    print(wynik)
 
 init()
-# def oblicz(dzialanie,x,y)
-#     if dzialanie == "dodawianie":
-#         return  x+y
-#     elif dzialanie == "odejmowanie":
-#         return x-y
-#     elif dzialanie == "mnozenie":
-#         return x * y
-#     elif dzialanie == "dzielenie":
-#         return  x/y
-
-
