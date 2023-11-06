d604af4c379f2954a34304e4582e8868571aa10a
DPulnar <w69997@student.wsiz.edu.pl>
2023-11-06 13:13:42 +0100

    zad2

--- lab2/innezadania/zad2.py
+++ lab2/innezadania/zad2.py
@@ -5,8 +5,8 @@
 def init():
     start = -4.0
     while start<=4:
-        wynik = (start*4)-5*start-8
-        print(wynik)
+        wynik = 2*start**2-5*start-8
+        print(f"dla x=={start}",wynik)
         start+=0.5
 
 
