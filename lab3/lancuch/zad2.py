0eb0d75975e63223d99f55f9dab9d85a481a0ca2
DPulnar <w69997@student.wsiz.edu.pl>
2023-11-13 13:20:51 +0100

    zad 2 lancuch zmiana v2

--- lab3/lancuch/zad2.py
+++ lab3/lancuch/zad2.py
@@ -4,14 +4,16 @@ tekst = "python jest super"
 print(tekst[0])
 print(tekst[len(tekst)-1])
 print("CO DRUGI")
-for i in range(0,len(tekst),2):
-    print(tekst[i])
+for i in tekst[0:len(tekst):2]:
+    print(i)
 print("CO trzeci")
-for i in range(0, len(tekst), 3):
-    print(tekst[i])
+for i in tekst[0:len(tekst):3]:
+    print(i)
 print("OD DZIESIATEGO DO OSTATNIEGO")
-for i in range(10, -1, -1):
-    print(tekst[i],i)
+for i in tekst[9::-1]:
+    print(i)
+# for i in range(10, -1, -1):
+#     print(tekst[i],i)
 print("OD KONCA DO POCZATKU")
 # for i in range(len(tekst)-1, -1,-1):
 #     print(tekst[i],i)
