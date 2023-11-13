7ca211d7f326fdb9b181d1c4bc358c1e1b88b430
DPulnar <w69997@student.wsiz.edu.pl>
2023-11-13 12:00:47 +0100

    wejsciowka

---
+++ lab3/wejsciowka/zad1.py
@@ -0,0 +1,11 @@
+
+
+def init():
+    a = int(input("podaj a"))
+    b = int(input("podaj b"))
+
+    while a<=b:
+        if a%3 ==0:
+            print(a)
+        a+=1
+init()
---
+++ lab3/wejsciowka/zad2.py
@@ -0,0 +1,8 @@
+
+
+def init():
+    a = 1
+    while a>0:
+       a = int(input("podaj liczbe calkowita"))
+
+init()
