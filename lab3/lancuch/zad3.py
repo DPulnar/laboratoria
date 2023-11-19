2e3fa6e430c25f0a25d11ed4a1a0f73cd5ae2fad
zlodziejinternetu <d.pulnar@o2.pl>
2023-11-19 22:09:24 +0100

    zad 3 lancuch dokonczone

--- lab3/lancuch/zad3.py
+++ lab3/lancuch/zad3.py
@@ -1,22 +1,42 @@
 
 
 def init():
- x = input("podaj imie")
- print("Witaj "+x)
 
- x = input("podaj wiek")
- print("Twój wiek to "+x)
+ przyklad = input("podaj ktory przyklad")
+ if przyklad=="1":
+  x = input("podaj imie")
+  print("Witaj " + x)
+ elif przyklad=="2":
+  x = input("podaj wiek")
+  print("Twój wiek to " + x)
+ elif przyklad == "3":
+  x = input("Podaj imie")
+  y = input("Podaj nazwisko")
+  print(x[0] + y[0])
+ elif przyklad == "4":
+  x = input("Podaj lancuch")
+  for i in range(0, 5):
+   print(x)
+ elif przyklad == "5":
+  x = input("Podaj lancuch")
+  y = input("Podaj drugi lancuch")
+  z = x + y
+ elif przyklad == "6":
+  x = input("Podaj lancuch")
+  y = input("Podaj drugi lancuch")
+  z = x[:(len(x) // 2)] + y[(len(x) // 2):]
+  print(z)
 
- x = input("Podaj imie")
- y  = input("Podaj nazwisko")
- print(x[0]+y[0])
 
- x= input("Podaj lancuch")
- for i in range(0,5):
-  print(x)
 
- x = input("Podaj lancuch")
- y  = input("Podaj drugi lancuch")
- z = x+y
+
+
+
+
+
+
+
+
+
 
 init()
