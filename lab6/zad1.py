f534be861a691cb5994572ca95322ef6f688c586
zlodziejinternetu <d.pulnar@o2.pl>
2024-01-21 21:42:58 +0100

    podpunk b

--- lab6/zad1.py
+++ lab6/zad1.py
@@ -4,8 +4,9 @@ import random
 
 szczesliwy_numerek = random.randint(0,16)
 
-tablica = [16]
-
+tablica = [2002,2003,2004,2005]
+szczesliwy_rocznik = random.randint(0,len(tablica)-1)
+print(tablica[szczesliwy_rocznik])
 
 
 #C
